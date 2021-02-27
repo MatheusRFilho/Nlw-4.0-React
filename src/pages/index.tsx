65bd4ce8e85ebdda36aaa4ec6b4cea21f7042495
Matheus Filho <matheusrodrigofilho@outlook.com>
2021-02-27 06:41:10 -0300

    Criando o botão do componente countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,15 @@ import { Profile } from "../components/Profile";
 
 import styles from '../styles/pages/Home.module.css';
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Início | Move it</title>
+      </Head>
       
      <ExperienceBar />
      <section>
